c2ba3ffd2f5b322ebc981046fc6afb9880084bde
ShenYang98 <63714751+ShenYang98@users.noreply.github.com>
2021-12-24 13:54:24 +0800

    柱状图

--- Echarts/柱状图/5.横向柱状图.js
+++ Echarts/柱状图/5.横向柱状图.js
@@ -1,16 +1,16 @@
-let title = ["企业财产保险","企业财产保险","企业财产保险","企业财产保险","企业财产保险"]
-let value = [22,33,44,55,66];
+let title = ["企业财产保险", "企业财产保险", "企业财产保险", "企业财产保险", "企业财产保险"]
+let value = [22, 33, 44, 55, 66];
 let max = Math.max(...value);
 let borderData = Array(title.length).fill(max * 1.1)
-let colorList = ["#FF646C","#FF646C","#FF646C","#FF646C","#FF646C"]
+let colorList = ["#FF646C", "#FF646C", "#FF646C", "#FF646C", "#FF646C"]
 let option = {
-    backgroundColor:"",
+    backgroundColor: "",
     xAxis: {
         splitLine: {
             show: false,
         },
         axisLabel: {
-            show:false
+            show: false
         }
     },
     yAxis: {
@@ -39,7 +39,7 @@ let option = {
             type: "bar",
             data: value,
             itemStyle: {
-                color: function(params) {
+                color: function (params) {
                     return colorList[params.dataIndex]
                 }
             },
@@ -61,7 +61,7 @@ let option = {
                 position: "right",
                 fontSize: 6,
                 color: "RGBA(0,244,245,1)",
-                formatter:function (data) {
+                formatter: function (data) {
                     return value[data.dataIndex]
                 }
             },
