e412fe275d40e3f3ed0ff5cfa49f7fdd2fc91323
ShenYang98 <63714751+ShenYang98@users.noreply.github.com>
2022-01-07 14:14:21 +0800

    leetcode

--- 算法/LeetCode/1.两数之和.js
+++ 算法/LeetCode/1.两数之和.js
@@ -1,4 +1,14 @@
 function twoSum(nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = 0; j < i; j++) {
+            if (nums[i] + nums[j] == target) {
+                return [j, i]
+            }
+        }
+    }
+}
+
+function twoSum(nums, target) {
     const map = new Map();
     for (let i = 0; i < nums.length; i++) {
         const diff = target - nums[i];
